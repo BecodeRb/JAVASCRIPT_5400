93743db8fdcce136477e7eddb75377288c40ecdd
BecodeRb <128100092+BecodeRb@users.noreply.github.com>
2024-05-16 23:46:30 -0500

    Add files via upload

---
+++ pre_entrega3_Basanez/js/main.js
@@ -0,0 +1,15 @@
+
+
+// Inventario de pizzas
+const inventarioPizza = {
+    1: { producto: "Margarita Clásica", tipo: "Margarita", precio: 190 },
+    2: { producto: "Margarita Pesto", tipo: "Margarita", precio: 210 },
+    3: { producto: "Margarita New York", tipo: "Margarita", precio: 250 },
+    4: { producto: "Peperoni Clásica", tipo: "Pepperoni", precio: 210},
+    5: { producto: "Peperoni Chicago", tipo: "Pepperoni", precio: 320},
+    6: { producto: "Peperoni con anchoas", tipo: "Pepperoni", precio: 230},
+    7: { producto: "4 Quesos Clásica",tipo: "4 Quesos", precio: 200 },
+    8: { producto: "4 Quesos con Blue Cheese",tipo: "4 Quesos", precio: 220 },
+    9: { producto: "Jamon Clásica",tipo: "Jamon", precio: 210},
+    10: { producto: "Jamon Italiana",tipo: "Jamon", precio: 240},
+};
