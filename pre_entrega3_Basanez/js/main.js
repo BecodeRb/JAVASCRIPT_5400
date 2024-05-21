cd853df3efd10ed4d6e71724c4de830c849ea6fb
BecodeRb <bernardobasanez@hotmail.com>
2024-05-20 20:41:04 -0500

    filter, find, JSON

--- pre_entrega3_Basanez/js/main.js
+++ pre_entrega3_Basanez/js/main.js
@@ -1,90 +1,30 @@
 
+let productos = [];
 
-// Inventario de pizzas
-
-/*
-const inventarioPizza = {
-    1: { producto: "Margarita Clásica", tipo: "Margarita", precio: 190 },
-    2: { producto: "Margarita Pesto", tipo: "Margarita", precio: 210 },
-    3: { producto: "Margarita New York", tipo: "Margarita", precio: 250 },
-    4: { producto: "Peperoni Clásica", tipo: "Pepperoni", precio: 210},
-    5: { producto: "Peperoni Chicago", tipo: "Pepperoni", precio: 320},
-    6: { producto: "Peperoni con anchoas", tipo: "Pepperoni", precio: 230},
-    7: { producto: "4 Quesos Clásica",tipo: "4 Quesos", precio: 200 },
-    8: { producto: "4 Quesos con Blue Cheese",tipo: "4 Quesos", precio: 220 },
-    9: { producto: "Jamon Clásica",tipo: "Jamon", precio: 210},
-    10: { producto: "Jamon Italiana",tipo: "Jamon", precio: 240},
-};
-
-*/
-
-// PRODUCTOS
-const productos = [
-    // Pizzas
-    {
-        id: "Pizza Margarita",
-        titulo: "Margarita Clásica",
-        imagen: ".//img/productos/pizza_margarita_clasica-01.jpg",
-        categoria: {
-            nombre: "Margarita",
-            id: "margarita"
-        },
-        precio: 190
-    },
-    {
-        id: "Pizza Margarita",
-        titulo: "Margarita Pesto",
-        imagen: ".//img/productos/pizza_margarita_clasica-01.jpg",
-        categoria: {
-            nombre: "Margarita",
-            id: "margarita"
-        },
-        precio: 210
-    },
-    {
-        id: "Pizza Margarita",
-        titulo: "Margarita New York",
-        imagen: ".//img/productos/pizza_margarita_clasica-01.jpg",
-        categoria: {
-            nombre: "Margarita",
-            id: "margarita"
-        },
-        precio: 250
-    },
-    {
-        id: "Pizza Pepperoni",
-        titulo: "Peperoni Clásica",
-        imagen: ".//img/productos/pizza_margarita_clasica-01.jpg",
-        categoria: {
-            nombre: "Pepperoni",
-            id: "pepperoni"
-        },
-        precio: 210
-    },
-    {
-        id: "Pizza Pepperoni",
-        titulo: "Peperoni Chicago",
-        imagen: ".//img/productos/pizza_margarita_clasica-01.jpg",
-        categoria: {
-            nombre: "Pepperoni",
-            id: "pepperoni"
-        },
-        precio: 320
-    }
-    
-];
+fetch(".//productos.json")
+    .then(response => response.json())
+    .then(data => {
+        productos = data;
+        cargarProductos(productos);
+    })
 
 //lamamos con DOM (DOCUMENT OBJECT MODEL)
 const contenedorProductos = document.querySelector("#contenedor-productos");
 
+const botonesCategorias = document.querySelectorAll(".boton-categoria");
+const tituloPrincipal = document.querySelector(".titulo-principal");
+
 
 
 //funcion para llamar productos de array
-function cargarProductos(){
+function cargarProductos(productosElegidos){
 
-    productos.forEach(producto => {
+    //vacia productos a mostrar
+    contenedorProductos.innerHTML = "";
 
-         let div = document.createElement("div")
+    productosElegidos.forEach(producto => {
+
+        const div = document.createElement("div")
 
         div.classList.add("producto");
         div.innerHTML = `
@@ -96,13 +36,41 @@ function cargarProductos(){
         </div>
     `;
 
-    contenedorProductos.append(div)
+    contenedorProductos.append(div);
 
     })
 
 }
 
-cargarProductos();
+cargarProductos(productos);
+
+botonesCategorias.forEach(boton => {
+    boton.addEventListener("click", (e) => {
+
+        botonesCategorias.forEach(boton => boton.classList.remove("active"));
+
+        e.currentTarget.classList.add("active");
+
+//para cargar todos los productos 
+if (e.currentTarget.id != "todos") {
+    //encontrar nombre categoria para mostrar
+    const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id);
+    tituloPrincipal.innerText = productoCategoria.categoria.nombre;
+
+    //filtrar categorias para cargar
+    const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
+    cargarProductos(productosBoton);
+} else {
+    tituloPrincipal.innerText = "Todos los productos";
+    cargarProductos(productos);
+}
+        
+
+
+    })
+})
+
+
 
 /*
 
