ade9ab35a14573a07831d321fe453d25181e37af
BecodeRb <bernardobasanez@hotmail.com>
2024-06-01 08:53:44 -0500

    funcionalidad carrito y formulario validacion
    funcionalidad botones en categorias y sumar carrito, y creacion de carrito con funcionalidad sumar productos y sumar totales, así como vaciar carrito

---
+++ entrega_final_Basanez/js/carrito.js
@@ -0,0 +1,274 @@
+const apellido_correcto = "MESSI";
+const habitacion_correcta = 280;
+
+const alertaAgregandoProducto = (operacion, mensaje) => {
+  const color = operacion == "+" ? "#4fbe87" : "#e74c3c";
+
+  Toastify({
+    text: mensaje,
+    duration: 3000,
+    gravity: "top",
+    position: "right",
+    style: {
+      background: color,
+    },
+  }).showToast();
+};
+
+/* const pagar = () => {
+  let apellido = document.getElementById("apellido");
+  let habitacion = document.getElementById("habitacion");
+  const carrito = obtenerCarrito();
+
+
+  if (apellido.value == apellido_correcto) {
+    apellido.classList.remove("is-invalid");
+    apellido.classList.add("is-valid");
+  } else {
+    apellido.classList.remove("is-valid");
+    apellido.classList.add("is-invalid");
+  }
+
+  if (habitacion.value == habitacion_correcta) {
+    habitacion.classList.remove("is-invalid");
+    habitacion.classList.add("is-valid");
+  } else {
+    habitacion.classList.remove("is-valid");
+    habitacion.classList.add("is-invalid");
+  }
+  if (
+    apellido.value == apellido_correcto &&
+    habitacion.value == habitacion_correcta && carrito.length > 0
+  ) {
+    eliminarProductoDelCarrito();
+    alertaAgregandoProducto("+", "Compra cargada a la Habitacion");
+    
+  }
+}; */
+
+// refactorizacion de codigo pagar
+
+const resetearFormulario = () => {
+  let apellido = document.getElementById("apellido");
+  let habitacion = document.getElementById("habitacion");
+
+  apellido.value = "";
+  habitacion.value = "";
+
+  removerClase(apellido, "is-valid");
+  removerClase(apellido, "is-invalid");
+  removerClase(habitacion, "is-valid");
+  removerClase(habitacion, "is-invalid");
+};
+
+const agregarClase = (elemento, clase) => {
+  elemento.classList.add(clase);
+};
+
+const removerClase = (elemento, clase) => {
+  elemento.classList.remove(clase);
+};
+
+const validarElemento = (elemento, valor_correcto) => {
+  if (elemento.value == valor_correcto) {
+    removerClase(elemento, "is-invalid");
+    agregarClase(elemento, "is-valid");
+    return true;
+  } else {
+    removerClase(elemento, "is-valid");
+    agregarClase(elemento, "is-invalid");
+    return false;
+  }
+};
+
+const pagar = () => {
+  let apellido = document.getElementById("apellido");
+  let habitacion = document.getElementById("habitacion");
+  const carrito = obtenerCarrito();
+
+  if (carrito.length) {
+    const apellidoValido = validarElemento(apellido, apellido_correcto);
+    const habitacionValida = validarElemento(habitacion, habitacion_correcta);
+
+    if (apellidoValido && habitacionValida && carrito.length > 0) {
+      eliminarProductoDelCarrito();
+      alertaAgregandoProducto("+", "Compra cargada a la Habitacion");
+      resetearFormulario();
+    }
+  } else {
+    alertaAgregandoProducto("-", "El carrito está vacío");
+  }
+};
+
+const obtenerCarrito = () => {
+  const carrito = localStorage.getItem("carrito");
+
+  return carrito ? JSON.parse(carrito) : [];
+};
+
+const guardarCarrito = (carrito) => {
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+
+  obtenerPizzas();
+};
+
+const eliminarProductoDelCarrito = (id) => {
+  if (id == undefined) {
+    // Eliminar el carrito del localStorage
+    const carrito = obtenerCarrito();
+    if (carrito.length > 0) {
+      localStorage.removeItem("carrito");
+      obtenerPizzas();
+    }
+  } else {
+    // Obtener el carrito del localStorage
+    let carrito = obtenerCarrito();
+
+    // Filtrar el carrito para eliminar el producto con el id especificado
+    carrito = carrito.filter((producto) => producto.id != id);
+    // Guardar el carrito actualizado en el localStorage
+    guardarCarrito(carrito);
+    alertaAgregandoProducto("-", "Producto eliminado del carrito");
+  }
+};
+
+const vaciarCarrito = () => {
+  const carrito = obtenerCarrito();
+  if (carrito.length > 0) {
+    eliminarProductoDelCarrito();
+    alertaAgregandoProducto("-", "El carrito está vacío");
+  }
+};
+
+const obtenerPizzas = () => {
+  // Llama a esta función para actualizar el total del carrito en la interfaz de usuario
+
+  fetch("js/productos.json")
+    .then((response) => {
+      return response.json();
+    })
+    .then((productos) => {
+      //LLAMAR AL ID DEL HTML
+      const elemento_html_productos = document.getElementById("carrito");
+
+      let contenido = "";
+      //obtenemos el valor del localstorage
+      let carrito = obtenerCarrito();
+
+      let total = 0;
+      // FUNCION PARA PINTAR PRODUCTOS EN HTML
+      console.log(carrito);
+      carrito.forEach((producto_carrito) => {
+        /*  productos.find(producto => producto.id === id); */
+        let producto = productos.find(
+          (producto) => producto.id == producto_carrito.id
+        );
+        producto.cantidad = producto_carrito.cantidad;
+        producto.subtotal = producto_carrito.cantidad * producto.precio;
+
+        total += producto.subtotal;
+        contenido += componenteProducto(producto);
+      });
+
+      elemento_html_productos.innerHTML = contenido;
+
+      total > 0
+        ? componenteTotal(`$${total} MXN`)
+        : componenteTotal("$0.00 MXN");
+    })
+
+    //avisa de errror si existe alguno en JSON por consola
+    .catch((error) => {
+      console.error("error en el json de productos", error);
+    });
+};
+
+obtenerPizzas();
+
+// Función para agregar o actualizar un item en el carrito
+const actulizarCarrito = (id, operacion) => {
+  let carrito = obtenerCarrito();
+
+  //documentacion findIndex https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/findIndex
+  const pizza_id = carrito.findIndex((pizza) => pizza.id == id);
+
+  if (pizza_id != -1) {
+    // Si la pizza existe, actualiza la cantidad
+    if (operacion == "+") {
+      //agregamos elementos a la pizza
+      carrito[pizza_id].cantidad += 1;
+      alertaAgregandoProducto("+", "Producto agregado del carrito");
+    } else if (operacion == "-") {
+      //quitamos elementos a la pizza
+      carrito[pizza_id].cantidad -= 1;
+      // Si la cantidad es cero o menor, remueve la pizza del carrito
+      if (carrito[pizza_id].cantidad <= 0) {
+        carrito.splice(pizza_id, 1);
+      }
+
+      alertaAgregandoProducto("-", "Producto eliminado del carrito");
+    }
+  } else {
+    // Si la pizza no existe, agrega uno nuevo
+
+    if (operacion == "+") {
+      carrito.push({ id, cantidad: 1 });
+      alertaAgregandoProducto("+", "Producto agregado del carrito");
+    }
+  }
+
+  // Guarda el carrito actualizado en localStorage
+  guardarCarrito(carrito);
+};
+
+const componenteTotal = (texto) => {
+  document.getElementById("total").innerHTML = texto;
+};
+
+const componenteProducto = (producto) => {
+  return `<div class="card mb-3">
+    <div class="row g-0">
+        <div class="col-md-1">
+            <img src="${producto.imagen}"
+                class="img-fluid rounded-start img-product" alt="...">
+        </div>
+        <div class=" col-md-4 align-content-space-evenly">
+
+            <div class="p-2 ">
+                <p class="p-0 m-0">Titulo</p>
+                <strong>Margarita clasica</strong>
+            </div>
+
+
+        </div>
+        <div class="col-md-2 align-content-space-evenly text-center">
+            <div class="p-2 ">
+                <p class="p-0 m-0">Cantidad</p>
+                <div class="input-group">
+                    <button class="btn btn-outline-secondary btn-main-color" type="button"
+                        id="button-addon1"  onClick="actulizarCarrito(${producto.id},'-')">-</button>
+                    <input type="text" class="form-control text-center" min="0" value="${producto.cantidad}" disabled>
+                    <button class="btn btn-outline-secondary btn-main-color" type="button"  onClick="actulizarCarrito(${producto.id},'+')">+</button>
+                </div>
+            </div>
+
+        </div>
+        <div class="col-md-2 align-content-space-evenly">
+
+            <div class="p-2 ">
+                <p class="p-0 m-0">Precio</p>
+                <strong>$${producto.precio} MXN</strong>
+            </div>
+        </div>
+        <div class="col-md-2 align-content-space-evenly">
+            <div class="p-2 ">
+                <p class="p-0 m-0">Subtotal</p>
+                <strong>$${producto.subtotal} MXN</strong>
+            </div>
+        </div>
+        <div class="col-md-1 align-content-space-evenly text-center">
+            <button class="btn-trash btn bi bi-trash-fill" onClick="eliminarProductoDelCarrito(${producto.id})" ></button>
+        </div>
+    </div>
+</div>`;
+};
--- entrega_final_Basanez/js/main.js
+++ entrega_final_Basanez/js/main.js
@@ -1,93 +1,208 @@
-//verificar que el DOM este cargado totalmente
-document.addEventListener("DOMContentLoaded", () => {
-    // Inventario de pizzas
-    /*
-  const inventario_pizza = {
-      1: { producto: "Margarita Clásica", tipo: "Margarita", precio: 190 },
-      2: { producto: "Margarita Pesto", tipo: "Margarita", precio: 210 },
-      3: { producto: "Margarita New York", tipo: "Margarita", precio: 250 },
-      4: { producto: "Peperoni Clásica", tipo: "Pepperoni", precio: 210},
-      5: { producto: "Peperoni Chicago", tipo: "Pepperoni", precio: 320},
-      6: { producto: "Peperoni con anchoas", tipo: "Pepperoni", precio: 230},
-      7: { producto: "4 Quesos Clásica",tipo: "4 Quesos", precio: 200 },
-      8: { producto: "4 Quesos con Blue Cheese",tipo: "4 Quesos", precio: 220 },
-      9: { producto: "Jamon Clásica",tipo: "Jamon", precio: 210},
-      10: { producto: "Jamon Italiana",tipo: "Jamon", precio: 240},
-  */
-  
-    window.obtenerPizzas = (categoria_id) => {
-      fetch("js/productos.json")
-        .then((response) => {
-          return response.json();
-        })
-        .then((productos) => {
-          const elemento_html_productos = document.getElementById("productos");
-  
-          let contenido = "";
-  
-          productos.forEach((producto) => {
-            if (categoria_id == undefined) {
-              contenido += vistaTarjetaProducto(producto);
-            } else {
-              if (producto.categoria.id == categoria_id) {
-                contenido += vistaTarjetaProducto(producto);
-              }
-            }
-          });
-  
-          elemento_html_productos.innerHTML = contenido;
-        })
-        .catch((error) => {
-          console.error("error en el json de productos", error);
-        });
-    };
-  
-    obtenerPizzas();
-  
-    const obtenerCategorias = () => {
-      fetch("js/categorias.json")
-        .then((response) => {
-          return response.json();
-        })
-        .then((categorias) => {
-          const elemento_html_catagoria = document.getElementById("categorias");
-  
-          let contenido = "";
-  
-          categorias.forEach((categoria) => {
-            contenido += vistaBotonCategoria(categoria);
-          });
-          //elemento_html_catagoria.innerHTML = contenido;
-  
-          //solucion para poner los productos abajo de "Todos del producto" https://developer.mozilla.org/es/docs/Web/API/Element/insertAdjacentHTML
-          elemento_html_catagoria.insertAdjacentHTML("beforeend", contenido);
-        })
-        .catch((error) => {
-          console.error("error en el json de categorias", error);
-        });
-    };
-  
-    obtenerCategorias();
-  
-    const vistaBotonCategoria = (data) => {
-      return `<button type="button" class="list-group-item list-group-item-action btn-main-color" onClick="obtenerPizzas(${data.id})" >${data.nombre}</button>`;
-    };
-  
-    const vistaTarjetaProducto = (data) => {
-      return ` <div class="col-4">
-      <div class="card  mb-2">
-        <img src="./img/productos/pizza_margarita_clasica-01.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">${data.producto}</h5>
-          <p class="card-text">${data.categoria.nombre}</p>
-          <p class="card-text">$${data.precio} MXN</p>
-          <div class="input-group">
-            <button class="btn btn-outline-secondary btn-main-color" type="button" id="button-addon1">-</button>
-            <input type="text" class="form-control text-center" min="0" value="0" disabled>
-            <button class="btn btn-outline-secondary btn-main-color" type="button">+</button>
+const obtenerCarrito = () => {
+  const carrito = localStorage.getItem("carrito");
+
+  /*   if (carrito) {
+    return JSON.parse(carrito);
+  } else {
+    return [];
+  } */
+
+  //refactor
+  return carrito ? JSON.parse(carrito) : [];
+};
+
+const alertaAgregandoProducto = (operacion, mensaje) => {
+  const color =
+    operacion == "+"
+      ? "#4fbe87"
+      : "#e74c3c";
+
+  Toastify({
+    text: mensaje,
+    duration: 3000,
+    gravity: "top",
+    position: "right",
+    style: {
+      background: color,
+    },
+  }).showToast();
+};
+
+const guardarCarrito = (carrito) => {
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+};
+
+const mostrarCantidadTotal = () => {
+  const carrito = obtenerCarrito();
+  let cantidad_total = 0;
+
+  // Recorrer el carrito y sumar las cantidades
+  carrito.forEach((item) => {
+    cantidad_total += item.cantidad;
+  });
+
+  document.getElementById("total_carrito").innerHTML = cantidad_total;
+};
+
+// Función para agregar o actualizar un item en el carrito
+const actulizarCarrito = (id, operacion) => {
+  let carrito = obtenerCarrito();
+
+  //documentacion findIndex https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/findIndex
+  const pizza_id = carrito.findIndex((pizza) => pizza.id == id);
+
+  if (pizza_id != -1) {
+    // Si la pizza existe, actualiza la cantidad
+    if (operacion == "+") {
+      //agregamos elementos a la pizza
+      carrito[pizza_id].cantidad += 1;
+      alertaAgregandoProducto("+", "Producto agregado del carrito");
+    } else if (operacion == "-") {
+      //quitamos elementos a la pizza
+      carrito[pizza_id].cantidad -= 1;
+      // Si la cantidad es cero o menor, remueve la pizza del carrito
+      if (carrito[pizza_id].cantidad <= 0) {
+        carrito.splice(pizza_id, 1);
+      }
+
+      alertaAgregandoProducto("-", "Producto eliminado del carrito");
+    }
+  } else {
+    // Si la pizza no existe, agrega uno nuevo
+
+    if (operacion == "+") {
+      carrito.push({ id, cantidad: 1 });
+      alertaAgregandoProducto("+", "Producto agregado del carrito");
+    }
+  }
+
+  // Guarda el carrito actualizado en localStorage
+  guardarCarrito(carrito);
+
+  // Actualiza el valor del input correspondiente
+  const input_element = document.getElementById(`pizza-${id}`);
+  //verifico si existe el elemento para poder actulizar el input
+  if (input_element) {
+    const _pizza_id = carrito.find((pizza) => pizza.id == id);
+
+    input_element.value = _pizza_id ? _pizza_id.cantidad : 0;
+  }
+  // Llama a esta función para actualizar el total del carrito en la interfaz de usuario
+  mostrarCantidadTotal();
+};
+
+// Inventario de pizzas
+/*
+const inventario_pizza = {
+  1: { producto: "Margarita Clásica", tipo: "Margarita", precio: 190 },
+  2: { producto: "Margarita Pesto", tipo: "Margarita", precio: 210 },
+  3: { producto: "Margarita New York", tipo: "Margarita", precio: 250 },
+  4: { producto: "Peperoni Clásica", tipo: "Pepperoni", precio: 210},
+  5: { producto: "Peperoni Chicago", tipo: "Pepperoni", precio: 320},
+  6: { producto: "Peperoni con anchoas", tipo: "Pepperoni", precio: 230},
+  7: { producto: "4 Quesos Clásica",tipo: "4 Quesos", precio: 200 },
+  8: { producto: "4 Quesos con Blue Cheese",tipo: "4 Quesos", precio: 220 },
+  9: { producto: "Jamon Clásica",tipo: "Jamon", precio: 210},
+  10: { producto: "Jamon Italiana",tipo: "Jamon", precio: 240},
+*/
+
+//PRODUCTOS
+
+//BAJAR PRODUCTOS DE JSON ("API LOCAL")
+const obtenerPizzas = (categoria_id) => {
+  // Llama a esta función para actualizar el total del carrito en la interfaz de usuario
+  mostrarCantidadTotal();
+
+  fetch("js/productos.json")
+    .then((response) => {
+      return response.json();
+    })
+    .then((productos) => {
+      //LLAMAR AL ID DEL HTML
+      const elemento_html_productos = document.getElementById("productos");
+
+      let contenido = "";
+      //obtenemos el valor del localstorage
+      let carrito = obtenerCarrito();
+      // FUNCION PARA PINTAR PRODUCTOS EN HTML
+
+      productos.forEach((producto) => {
+        //verificamos si la pizza esta agregado al carrito
+        const pizza_id = carrito.findIndex((pizza) => pizza.id == producto.id);
+        //si se encuentra agregada modificamos el valor del input si no le agregamos 0
+        let valor = pizza_id != -1 ? carrito[pizza_id].cantidad : 0;
+
+        if (categoria_id == undefined) {
+          contenido += componenteTarjetaProducto(producto, valor);
+        } else {
+          if (producto.categoria.id == categoria_id) {
+            contenido += componenteTarjetaProducto(producto, valor);
+          }
+        }
+      });
+
+      elemento_html_productos.innerHTML = contenido;
+    })
+
+    //avisa de errror si existe alguno en JSON por consola
+    .catch((error) => {
+      console.error("error en el json de productos", error);
+    });
+};
+
+obtenerPizzas();
+
+//-----------------------//
+
+//CATEGORIAS
+
+//BAJAR CATEGORIAS DE JSON ("API LOCAL")
+const obtenerCategorias = () => {
+  fetch("js/categorias.json")
+    .then((response) => {
+      return response.json();
+    })
+    .then((categorias) => {
+      //LLAMAR AL ID DEL HTML
+      const elemento_html_catagoria = document.getElementById("categorias");
+
+      let contenido = "";
+
+      categorias.forEach((categoria) => {
+        // funcion para pintar categorias en html con la info de la categoria correspondiente
+        contenido += componenteBotonCategoria(categoria);
+      });
+      //elemento_html_catagoria.innerHTML = contenido;
+
+      //solucion para poner los productos abajo de "Todos del producto" https://developer.mozilla.org/es/docs/Web/API/Element/insertAdjacentHTML
+      elemento_html_catagoria.insertAdjacentHTML("beforeend", contenido);
+    })
+    //AVISA SI HAY ERROR EN JSON DE CATEGORIAS
+    .catch((error) => {
+      console.error("error en el json de categorias", error);
+    });
+};
+
+obtenerCategorias();
+
+const componenteBotonCategoria = (data) => {
+  return `<button type="button" class="list-group-item list-group-item-action btn-main-color" onClick="obtenerPizzas(${data.id})" >${data.nombre}</button>`;
+};
+
+const componenteTarjetaProducto = (data, cantidad) => {
+  return ` <div class="col-4">
+        <div class="card  mb-2">
+          <img src="./img/productos/pizza_margarita_clasica-01.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${data.producto}</h5>
+            <p class="card-text">${data.categoria.nombre}</p>
+            <p class="card-text">$${data.precio} MXN</p>
+            <div class="input-group">
+              <button class="btn btn-outline-secondary btn-main-color" type="button" id="button-addon1" onClick="actulizarCarrito(${data.id},'-')">-</button>
+              <input  id="pizza-${data.id}" type="text" class="form-control text-center" min="0" value="${cantidad}" disabled>
+              <button class="btn btn-outline-secondary btn-main-color" type="button" onClick="actulizarCarrito(${data.id},'+')">+</button>
+            </div>
           </div>
         </div>
-      </div>
-    </div>`;
-    };
-  });
+      </div>`;
+};
